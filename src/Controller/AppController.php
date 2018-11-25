41f49d77f62b428d9863a4e334212c43258b8447
RobinCamarasa <robin.camarasa@etu.emse.fr>
2018-11-25 16:41:41 +0100

    First version of the front web

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -12,8 +12,47 @@ class AppController extends Controller
      */
     public function index()
     {
-        return $this->render('app/index.html.twig', [
+        return $this->render('base.html.twig', [
             'data' => ['name' => 'Robin'],
         ]);
+    }
+
+    public function home()
+    {
+        return $this->render('app/home.html.twig', [
+            'data' => ['name' => 'Robin'],
+        ]);
+    }
+
+    public function profil()
+    {
+        return $this->render('app/profil.html.twig', [
+            'data' => ['name' => 'Robin'],
+        ]);
+    }
+
+    public function postit()
+    {
+        return $this->render('app/postit.html.twig');
+    }
+
+    public function courses()
+    {
+        return $this->render('app/courses.html.twig');
+    }
+
+    public function finances()
+    {
+        return $this->render('app/finances.html.twig');
+    }
+
+    public function taches()
+    {
+        return $this->render('app/taches.html.twig');
+    }
+
+    public function connexion()
+    {
+        return $this->render('app/connexion.html.twig');
     }
 }
